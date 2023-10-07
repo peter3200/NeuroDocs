4a007a64696066d1d016fef99a63955baec1b30e
peter3200 <40050100+peter3200@users.noreply.github.com>
2023-10-07 14:35:05 -0600

    Add files via upload

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,6 +42,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
